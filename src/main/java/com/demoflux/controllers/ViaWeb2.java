1ff0b2f119ca130cfb02b152cee92eb2e2f7780d
singgihcp <satenterus@gmail.com>
2022-08-09 01:31:02 +0700

    Update ViaWeb2.java

--- src/main/java/com/demoflux/controllers/ViaWeb2.java
+++ src/main/java/com/demoflux/controllers/ViaWeb2.java
@@ -2,6 +2,7 @@ package com.demoflux.controllers;
 
 public class ViaWeb2 {
   //ubah dari web 1
-    // ubah dari local
-//ubah dari web 2
+  // ubah dari local
+  //ubah dari web 2
+  //ubah dari web 3
 }
